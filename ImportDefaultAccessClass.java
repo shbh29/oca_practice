f15cac3dc9e6a184b2d362c184fbbee763a4beb3
Shubham <shbh29@gmail.com>
2019-10-21 20:08:10 +0530

    class containing main method need not be public

---
+++ ImportDefaultAccessClass.java
@@ -0,0 +1,7 @@
+import noclasspublic.RunThis;
+
+class ImportDefaultAcessClass{
+	public static void main(String... args){
+		RunThis.main(new String[]{"",""});
+	}
+}
---
+++ noclasspublic/RunThis.java
@@ -0,0 +1,7 @@
+package noclasspublic;
+
+class RunThis{
+	public static void main(String... args){
+		System.out.println("Hello");
+	}
+}
